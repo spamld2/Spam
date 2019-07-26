cfed85d126f86b590670a6657b6e4b94c90ecf6f
SpamLD <53348237+SpamLD@users.noreply.github.com>
2019-07-27 00:55:50 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("604072458022289408")
+setInterval(function() {
+channel.send(`Spam credits Hack by MrDaMaR&Mahmoud|Spam credits Hack by MrDaMaR-Mahmoud=Spam credits Hack by MrDaMaR&Mahmoud|Spam credits Hack by MrDaMaR-Mahmoud`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
